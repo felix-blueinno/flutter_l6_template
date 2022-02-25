de90ebe85f325ba628f7913f00b02c3c5f55dc3b
Felix-Blueinno <felix@blueinnotechnology.com>
2022-02-25 12:24:26 +0800

    first commit

---
+++ android/app/src/main/kotlin/com/example/flutter_l6_template/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_l6_template
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,61 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
+import 'package:flutter/material.dart';
+
+main() => runApp(MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    ));
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[900],
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            CircleAvatar(
+              radius: 32,
+              backgroundImage: NetworkImage("https://picsum.photos/200"),
+            ),
+            SizedBox(height: 16),
+
+            // Label:
+            Text(
+              "Today's Trending",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 8),
+
+            // Date Label:
+            Text(
+              "Tuesday, Feb 22",
+              style: TextStyle(
+                color: Colors.grey,
+                fontSize: 12,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 16),
+
+            // Continue here:
+          ],
+        ),
+      ),
+    );
+  }
+}
